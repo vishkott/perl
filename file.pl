04204c256f7209dfc179ddca30ee7bb53e31c614
Vish Kottignahal <vishk@broadcom.com>
2015-02-09 19:52:25 -0800

    file input on command line

--- file.pl
+++ file.pl
@@ -1,6 +1,12 @@
 #opens a file and prints the contents line by line with the line number
 $fname = "./wishMe.pl"; 
-open fh, $fname;
+open fh, $fname or die "Error in open: $!";
 while(<fh>){
 print "Line number $. is: $_";
 }
+print "\n\n\n";
+
+while(<>){ #same as <ARGV>
+print;
+}
+
